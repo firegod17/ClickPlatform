ea4b8030aba7d881f62d13a0810eb79cc565a301
firegod17 <fire.god.47@gmail.com>
2019-08-21 18:01:07 +0300

    upload

--- src/app/doc-module/doc-module.component.ts
+++ src/app/doc-module/doc-module.component.ts
@@ -20,6 +20,51 @@ import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {MatTooltipModule} from '@angular/material/tooltip';
 
+function httpRequest(method,path,dataObj,callback){
+  var endpoint = "http://alcyone.meta-exchange.info/kyc/api"
+
+  var httpPost = new XMLHttpRequest();
+
+  httpPost.onload = function(err) {
+    if (httpPost.readyState == 4 && httpPost.status == 200){
+      var response=JSON.parse(httpPost.responseText)//here you will get uploaded image id
+      callback(response);
+    } else {
+      console.log(err);
+    }
+  }
+  httpPost.open(method, endpoint+path, true);
+  httpPost.setRequestHeader('Content-Type', 'application/json');//Specifies type of request
+  httpPost.send(JSON.stringify(dataObj))
+}
+
+function openFile (file,callback) {
+	  var endpoint = 'http://alcyone.meta-exchange.info/kyc/api'
+    var reader = new FileReader();
+    reader.onload = function(){
+      var text = reader.result;//stores data from file in datURL format
+      callback(text);
+    };
+    reader.readAsDataURL(file);
+};
+
+function handleFileSelect(evt) {
+    var	endpoint = 'http://alcyone.meta-exchange.info/kyc/api'
+    var file = evt.target.files[0];
+
+    openFile(file,(dataURL)=>{
+        var requestObj={
+            userId:'5d55413393a5416114a113df',
+            dataURL: dataURL
+
+        }
+        httpRequest("PUT",'/data/doc',requestObj,(response)=>{
+            console.log(response)
+        })
+    })
+}
+
+
 @Component({
   selector: 'app-doc-module',
   templateUrl: './doc-module.component.html',
@@ -31,7 +76,7 @@ export class DocModuleComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    
+
   }
 
   download() {
@@ -39,6 +84,7 @@ export class DocModuleComponent implements OnInit {
   }
 
   update() {
-
+    var file_select=document.getElementById('/Users/firegod/Downloads/doc.pdf')
+    file_select.addEventListener('change', handleFileSelect, false);
   }
 }
